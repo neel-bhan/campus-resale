8bedecdf9570e55571df4f52719b36d552031201
neel-bhan <neelbh99@gmail.com>
2025-11-05 22:13:44 -0600

    removed more user

--- frontend/src/pages/CreatePost.tsx
+++ frontend/src/pages/CreatePost.tsx
@@ -11,7 +11,7 @@ interface CreatePostProps {
   user: any;
 }
 
-export function CreatePost({ user: _user }: CreatePostProps) {
+export function CreatePost({  }: CreatePostProps) {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
